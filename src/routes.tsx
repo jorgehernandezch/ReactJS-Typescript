a7847c89948f96ba6672a0911762d755ed4d47ba
Jorge Edo. Hernández <jorge@galpha.co>
2021-08-03 22:49:16 -0300

    Routes organization

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Index } from './pages/index';
+import { Home } from './pages/Home/index';
 
 export const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Index}></Route>
+        <Route path="/" exact component={Home}></Route>
       </Switch>
     </BrowserRouter>
   );
