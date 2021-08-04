483a36d15193c00fad25f6ffa84a2f1730cbecbd
Jorge Edo. Hernández <jorge@galpha.co>
2021-08-03 22:50:05 -0300

    Menu component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import '../../assets/css/global.css';
+import './style.css';
+
+export const Header = () => {
+  return(
+    <header>
+      <div className="header">
+        <div className="brand">
+          <img src="https://redone.com.co/build/img/logo-alpha.png" alt="Logotipo" />
+        </div>
+        <nav>
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
